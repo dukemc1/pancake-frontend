4d1f45cd356cf76dde78395f98e2c21c422cb5d4
dukemc1 <crashoverride19@gmx.at>
2021-02-05 07:41:22 +0100

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -78,9 +78,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'PancakeSwap')}
+          {TranslateString(576, 'BSC Lottery')}
         </Heading>
-        <Text>{TranslateString(578, 'The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'Your 1st Lottery System on Binance Smart Chain!')}</Text>
       </Hero>
       <div>
         <Cards>
